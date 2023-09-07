c8a7d1421d810f166e11085afae36848b08f4f31
CD <141286004+crisdd0302@users.noreply.github.com>
2023-09-07 12:38:09 -0500

    index js con app.use

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const accountsController = require("./controllers/accounts");
+// const moviesController = require('./controllers/movies');
+
+app.use("/accounts", accountsController);
+// app.use('/accounts', moviesController);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
